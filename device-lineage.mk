285762f78bde9456cfcf4f41d546f32a3f442f4b
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-12 04:48:12 +0530

    bluejay: Set TARGET_PREBUILT_KERNEL
    Change-Id: I156a4af77bd619f855f734983b24677531156c4d

--- device-lineage.mk
+++ device-lineage.mk
@@ -15,6 +15,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# Kernel
+TARGET_PREBUILT_KERNEL := device/google/bluejay-kernel/Image.lz4
+
 # Build necessary packages for vendor
 
 # Audio
